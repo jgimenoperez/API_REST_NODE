f97565b9da2f86e2e7c7cb1cc05eb95ea12d2b19
jgimenoperez <jgimenoperez@gmail.com>
2020-10-07 09:16:24 +0200

    AÑADO LINK FAZT

--- src/app.js
+++ src/app.js
@@ -7,8 +7,10 @@ const bodyParser=require('body-Parser')
 
 
 //middleware
+app.set('json spaces', 4) //esablece espacios en la visualización por navegador
 app.use(morgan('dev'));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}))
 
 require('./routes/userRoutes')(app) ;
 
@@ -18,3 +20,4 @@ app.listen(app.get('port'),()=>{
     console.log('Server on port 3000');
 });
 
+//https://www.youtube.com/watch?v=QNBozn-zPhs&list=PLL0TiOXBeDaiaFOobfapYZ2ughvAnCTrW&index=4
